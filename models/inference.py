3484309f4aca3950d7a2e1527a3eae7c843acfc9
Your Name <you@example.com>
2019-12-12 17:44:02 +0900

    init

---
+++ models/inference.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 11 21:36:35 2019
+
+@author: yk6673
+"""
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.preprocessing import image
+
+def load_image(image_path, grayscale = False, target_size=None):
+    pil_image = image.load_img(image_path, grayscale, target_size)
+    return image.img_to_array(pil_image)
+
+def load_detection_model(model_path):
+    detection_model = cv2.CascadeClassifier(model_path)
+    return detection_model
+    
+def detect_faces(detection_model, gray_image_array):
+    return detection_model.detectMultiScale(gray_image_array, 1.3, 5)
+
+def apply_offsets(face_coordinates, offsets):
+    x, y, width, height = face_coordinates
+    x_off, y_off = offsets
+    return (x- x_off, x+width+x_off, y-y_off, y+height+y_off)
